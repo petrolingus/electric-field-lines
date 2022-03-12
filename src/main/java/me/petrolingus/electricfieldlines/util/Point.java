762c530c16da5056a51a003089f7889a47921435
petrolingus <petrolingus@gmail.com>
2022-03-12 11:34:22 +0300

    Deleted unused class

--- src/main/java/me/petrolingus/electricfieldlines/util/Point.java
+++
@@ -1,9 +0,0 @@
-package me.petrolingus.electricfieldlines.util;
-
-import javafx.scene.paint.Color;
-
-public record Point(double x, double y, Color color) {
-    double mod() {
-        return x * x + y * y;
-    }
-}
